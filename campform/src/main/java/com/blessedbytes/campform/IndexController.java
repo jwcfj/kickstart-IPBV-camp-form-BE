c4f01c86217afd90ffed370df6b1007570a1455d
Rodrigo Moura <rrm2@cin.ufpe.br>
2023-08-18 01:19:54 -0300

    Receiving JSON, turning it into an object and turning the object into a JSON string

--- campform/src/main/java/com/blessedbytes/campform/IndexController.java
+++ campform/src/main/java/com/blessedbytes/campform/IndexController.java
@@ -1,9 +1,16 @@
 package com.blessedbytes.campform;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.blessedbytes.campform.models.Person;
 
@@ -20,17 +27,22 @@ public class IndexController {
     @ResponseBody
     public Person get() {
         Person data = new Person();
-        // Preencha o objeto com os dados
         return data;
     }
 
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public String post(){
+	@RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String post(@RequestBody Person jsonRequest) throws JsonProcessingException{
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(jsonRequest);
+        System.out.println(json);
 		return "post";
 	}
 
-	@RequestMapping(value="/", method=RequestMethod.DELETE)
-	public String delete(){
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public String delete(@PathVariable String id){
+
+		System.out.println("ID recebido: " + id);
+
 		return "delete";
 	}
 }
