e26c83595ea7514ab797025eb7b01737b4086733
jwcfj <jwcfj@gmail.com>
2023-08-17 23:03:03 -0300

    api initial config

--- campform/src/main/java/com/blessedbytes/campform/IndexController.java
+++ campform/src/main/java/com/blessedbytes/campform/IndexController.java
@@ -3,14 +3,26 @@ package com.blessedbytes.campform;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.blessedbytes.campform.models.Person;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	/*@RequestMapping(value="/", method=RequestMethod.GET)
 	public String get(){
+		//codigo que acessa csv
 		return "get";
-	}
+	}*/
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Person get() {
+        Person data = new Person();
+        // Preencha o objeto com os dados
+        return data;
+    }
 
 	@RequestMapping(value="/", method=RequestMethod.POST)
 	public String post(){
